b1445fe476917cd1f09733c407c287216fc1037b
RaqueSouto <76965176+RaqueSouto@users.noreply.github.com>
2024-05-23 19:15:37 +0200

    api colores en mongodb

---
+++ db.js
@@ -0,0 +1,74 @@
+require("dotenv").config();
+const { MongoClient,ObjectId } = require("mongodb");
+
+function conectar(){
+    return MongoClient.connect(process.env.URL_MONGO);
+}
+
+
+function leerColores(){
+    return new Promise(async (ok,ko) => {
+
+        try{
+            const conexion = await conectar();
+
+            let colores = await conexion.db("colores").collection("colores").find({}).toArray();
+
+            conexion.close();
+
+            ok(colores.map(({_id,r,g,b}) => {
+                return {id:_id,r,g,b};
+            }));
+
+        }catch(error){
+            ko({ error : "error en BBDD" });
+        }
+
+    });
+}
+
+
+function crearColor({r,g,b}){
+    return new Promise(async (ok,ko) => {
+        
+
+        try{
+            const conexion = await conectar();
+
+            let {insertedId} = await conexion.db("colores").collection("colores").insertOne({r,g,b});
+
+            conexion.close();
+
+            ok(insertedId);
+
+        }catch(error){
+            ko({ error : "error en BBDD" });
+            // console.log(error)
+        }
+
+    });
+}
+
+
+
+function borrarColor({id}){
+    return new Promise(async (ok,ko) => {
+        
+        try{
+            const conexion = await conectar();
+
+            let {deletedCount} = await conexion.db("colores").collection("colores").deleteOne({_id : new ObjectId(id)});
+
+            conexion.close();
+
+            ok(deletedCount);
+
+        }catch(error){
+            ko({ error : "error en BBDD" });
+        }
+
+    });
+}
+
+module.exports = {leerColores,crearColor,borrarColor};
+
